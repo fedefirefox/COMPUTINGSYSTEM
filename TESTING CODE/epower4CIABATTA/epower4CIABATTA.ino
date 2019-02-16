9cd484ce73519fd2dc90efa8d0f5a7440028f1cb
MartaGalbi <47305118+MartaGalbi@users.noreply.github.com>
2019-02-16 20:46:56 +0100

    Update epower4CIABATTA.ino

--- TESTING CODE/epower4CIABATTA/epower4CIABATTA.ino
+++ TESTING CODE/epower4CIABATTA/epower4CIABATTA.ino
@@ -1,9 +1,69 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <AltSoftSerial.h>
 
+AltSoftSerial altSerial;
+int turnon=0;
+int turnoff=0;
+
+void setup()  {
+
+  altSerial.begin(9600);//velocità voluta dalla ciabatta, 8 bit, no parity bit , 1 bit di stop
+  Serial.begin(9600);
+  
+ }
+
+void loop(){
+char response[100];
+int i;
+char respexpon[]={'P','0','0','=','1'};
+char respexpoff[]={'P','0','0','=','0'};
+//
+
+//codice di comunicazione che imposta i valori di turnon e turnoff;
+
+//
+
+
+
+if(turnon){
+
+  resendon:
+  altSerial.print("/P00=1");
+  delay(1000);
+  
+  if (altSerial.available()){
+  i=0;
+  while(altSerial.available() > 0 ){
+  response[i]=altSerial.read();
+  Serial.print(response[i]);
+  if(response[i]!=respexpon[i]){
+  goto resendon;}
+  }
+  
+  altSerial.print("\r");
+  turnon=0;
+  }
+   
+  
 }
+if(turnoff){
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+  resendoff:
+  altSerial.print("/P00=0");
+  delay(1000);
+  
+  if (altSerial.available()){
+  i=0;
+  while(altSerial.available() > 0 ){
+  response[i]=altSerial.read();
+  Serial.print(response[i]);
+  if(response[i]!=respexpoff[i]){
+  goto resendoff;}
+  }
+  
+  altSerial.print("\r");
+  turnoff=0;
+  }
+   
+ }  
+  
+} 
